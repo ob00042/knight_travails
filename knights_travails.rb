d883f01d8a8f1869ea56e01f9c6d55594a47c114
ob00042 <ob00042@gmail.com>
2016-07-18 17:09:39 +0300

    Refactored 1

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,10 +1,9 @@
 class Node
 
-  attr_accessor :value, :distance, :parent, :children
+  attr_accessor :value, :parent, :children
 
-  def initialize(value, distance, parent, children)
+  def initialize(value, parent, children)
   	@value = value
-  	@distance = distance
   	@parent = parent
   	@children = children
   end
@@ -13,53 +12,37 @@ end
 
 class Knight
 
-  attr_accessor :board
-
-  def create_board
-  	@board = []
-    x_axis = (0..7).to_a
-    y_axis = (0..7).to_a
-    x_axis.each do |x|
-      y_axis.each do |y|
-        @board << [x, y]
-      end
-    end
-    @board
-  end
 
   def knight_moves(start_box, target_box)
-  	@start_box = start_box
-  	@target_box = target_box
+  	@start = start_box
+  	@target = target_box
   	@move = [[1, 2], [2, 1], [-1, 2], [-1, -2], [1, -2], [-2, 1], [-2, -1], [2, -1]]
-  	start = Node.new(@start_box, nil, nil, nil)
-  	target = Node.new(target_box, 0, nil, nil)
-  	path = [@start_box]
+  	start = Node.new(start_box, nil, nil)
+  	target = Node.new(target_box, nil, nil)
   	@visited = [target]
   	@visited_values = [target_box]
   	@last = nil
   	queue = [target]
-  	dis = 0
   	until queue.empty?
   	  element = queue.shift
   	  @move.each do |move|
   	  	val = [element.value[0]+move[0], element.value[1]+move[1]]
-  	  	count = dis
-  	  	node = Node.new(val, count, element, nil)
+  	  	node = Node.new(val, element, nil)
   	  	if val[0]<8 && val[1]<8 && val[0]>-1 && val[1]>-1 && !@visited.include?(node) && !@visited_values.include?(val)
   	      element.children = node
   	      queue << node
-  	      @visited.unshift node
-  	      @visited_values.unshift val
+  	      @visited << node
+  	      @visited_values << val
   	      @last = node
   	      return if val == start_box
   	    end
   	  end
-  	  dis += 1
   	end
   
   end
 
   def visited_values
+  	puts "The moves from #{@start} to #{@target} were: "
   	p @last.value
   	node = @last
   	while node.parent
@@ -73,6 +56,5 @@ class Knight
 end
 
 kn = Knight.new
-kn.create_board
-kn.knight_moves([0, 0], [3,5])
+kn.knight_moves([0, 0], [7, 7])
 puts kn.visited_values
