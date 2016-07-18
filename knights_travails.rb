5735b99d43a524201c44ae6ed1cd5263425b940e
ob00042 <ob00042@gmail.com>
2016-07-18 17:01:23 +0300

    Complete

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,12 +1,12 @@
 class Node
 
-  attr_accessor :value, :parent, :children, :distance
+  attr_accessor :value, :distance, :parent, :children
 
-  def initialize(value, parent = nil, children = [], distance = nil)
+  def initialize(value, distance, parent, children)
   	@value = value
+  	@distance = distance
   	@parent = parent
   	@children = children
-  	@distance = distance
   end
 
 end
@@ -28,26 +28,29 @@ class Knight
   end
 
   def knight_moves(start_box, target_box)
+  	@start_box = start_box
+  	@target_box = target_box
   	@move = [[1, 2], [2, 1], [-1, 2], [-1, -2], [1, -2], [-2, 1], [-2, -1], [2, -1]]
-  	return "You are here!" if start_box == target_box
-  	start = Node.new(start_box)
-  	target = Node.new(target_box)
-  	path = [start_box]
+  	start = Node.new(@start_box, nil, nil, nil)
+  	target = Node.new(target_box, 0, nil, nil)
+  	path = [@start_box]
   	@visited = [target]
   	@visited_values = [target_box]
+  	@last = nil
   	queue = [target]
   	dis = 0
-  	while !queue.empty?
+  	until queue.empty?
   	  element = queue.shift
   	  @move.each do |move|
   	  	val = [element.value[0]+move[0], element.value[1]+move[1]]
-  	  	count = dis + 1
-  	  	node = Node.new(val, element, nil, count.to_i)
+  	  	count = dis
+  	  	node = Node.new(val, count, element, nil)
   	  	if val[0]<8 && val[1]<8 && val[0]>-1 && val[1]>-1 && !@visited.include?(node) && !@visited_values.include?(val)
   	      element.children = node
   	      queue << node
-  	      @visited << node
-  	      @visited_values << val
+  	      @visited.unshift node
+  	      @visited_values.unshift val
+  	      @last = node
   	      return if val == start_box
   	    end
   	  end
@@ -57,12 +60,19 @@ class Knight
   end
 
   def visited_values
-  	print @visited_values
+  	p @last.value
+  	node = @last
+  	while node.parent
+  	  previous = node.parent
+  	  p previous.value
+  	  node = previous
+  	end
+  	
   end
 
 end
 
 kn = Knight.new
 kn.create_board
-kn.knight_moves([0, 0], [1, 5])
+kn.knight_moves([0, 0], [3,5])
 puts kn.visited_values
